f9931c124e4e73adaa7ea4257ff9d52d71334e2a
qiaunna <shcla13@morgan.edu>
2022-01-17 17:38:34 -0500

    Update objdet_detect.py

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -2,7 +2,7 @@
 # Project "Track 3D-Objects Over Time"
 # Copyright (C) 2020, Dr. Antje Muntzinger / Dr. Andreas Haja.
 #
-# Purpose of this file : Detect 3D objects in lidar point clouds using deep learning
+# Purpose of this file : Process the point-cloud and prepare it for object detection
 #
 # You should have received a copy of the Udacity license together with this program.
 #
@@ -11,9 +11,11 @@
 #
 
 # general package imports
+import cv2
 import numpy as np
 import torch
-from easydict import EasyDict as edict
+import zlib
+import open3d
 
 # add project directory to python path to enable relative imports
 import os
@@ -22,173 +24,211 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-# model-related
-from tools.objdet_models.resnet.models import fpn_resnet
-from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing 
+# waymo open dataset reader
+from tools.waymo_reader.simple_waymo_open_dataset_reader import utils as waymo_utils
+from tools.waymo_reader.simple_waymo_open_dataset_reader import dataset_pb2, label_pb2
 
-from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
-from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
+# object detection tools and helper functions
+import misc.objdet_tools as tools
 
+# variable to control visualization
+visualize = False
 
-# load model-related parameters into an edict
-def load_configs_model(model_name='darknet', configs=None):
+# visualize lidar point-cloud
+def show_pcl(pcl):
 
-    # init config file, if none has been passed
-    if configs==None:
-        configs = edict()  
-
-    # get parent directory of this file to enable relative paths
-    curr_path = os.path.dirname(os.path.realpath(__file__))
-    parent_path = configs.model_path = os.path.abspath(os.path.join(curr_path, os.pardir))    
-    
-    # set parameters according to model type
-    if model_name == "darknet":
-        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'darknet')
-        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'complex_yolov4_mse_loss.pth')
-        configs.arch = 'darknet'
-        configs.batch_size = 4
-        configs.cfgfile = os.path.join(configs.model_path, 'config', 'complex_yolov4.cfg')
-        configs.conf_thresh = 0.5
-        configs.distributed = False
-        configs.img_size = 608
-        configs.nms_thresh = 0.4
-        configs.num_samples = None
-        configs.num_workers = 4
-        configs.pin_memory = True
-        configs.use_giou_loss = False
-
-    elif model_name == 'fpn_resnet':
-        ####### ID_S3_EX1-3 START #######     
-        #######
-        print("student task ID_S3_EX1-3")
-
-        #######
-        ####### ID_S3_EX1-3 END #######     
-
-    else:
-        raise ValueError("Error: Invalid model name")
-
-    # GPU vs. CPU
-    configs.no_cuda = True # if true, cuda is not used
-    configs.gpu_idx = 0  # GPU index to use.
-    configs.device = torch.device('cpu' if configs.no_cuda else 'cuda:{}'.format(configs.gpu_idx))
-
-    return configs
-
-
-# load all object-detection parameters into an edict
-def load_configs(model_name='fpn_resnet', configs=None):
-
-    # init config file, if none has been passed
-    if configs==None:
-        configs = edict()    
-
-    # birds-eye view (bev) parameters
-    configs.lim_x = [0, 50] # detection range in m
-    configs.lim_y = [-25, 25]
-    configs.lim_z = [-1, 3]
-    configs.lim_r = [0, 1.0] # reflected lidar intensity
-    configs.bev_width = 608  # pixel resolution of bev image
-    configs.bev_height = 608 
-
-    # add model-dependent parameters
-    configs = load_configs_model(model_name, configs)
-
-    # visualization parameters
-    configs.output_width = 608 # width of result image (height may vary)
-    configs.obj_colors = [[0, 255, 255], [0, 0, 255], [255, 0, 0]] # 'Pedestrian': 0, 'Car': 1, 'Cyclist': 2
-
-    return configs
-
-
-# create model according to selected model type
-def create_model(configs):
-
-    # check for availability of model file
-    assert os.path.isfile(configs.pretrained_filename), "No file at {}".format(configs.pretrained_filename)
-
-    # create model depending on architecture name
-    if (configs.arch == 'darknet') and (configs.cfgfile is not None):
-        print('using darknet')
-        model = darknet(cfgfile=configs.cfgfile, use_giou_loss=configs.use_giou_loss)    
-    
-    elif 'fpn_resnet' in configs.arch:
-        print('using ResNet architecture with feature pyramid')
-        
-        ####### ID_S3_EX1-4 START #######     
-        #######
-        print("student task ID_S3_EX1-4")
-
-        #######
-        ####### ID_S3_EX1-4 END #######     
-    
-    else:
-        assert False, 'Undefined model backbone'
-
-    # load model weights
-    model.load_state_dict(torch.load(configs.pretrained_filename, map_location='cpu'))
-    print('Loaded weights from {}\n'.format(configs.pretrained_filename))
-
-    # set model to evaluation state
-    configs.device = torch.device('cpu' if configs.no_cuda else 'cuda:{}'.format(configs.gpu_idx))
-    model = model.to(device=configs.device)  # load model to either cpu or gpu
-    model.eval()          
-
-    return model
-
-
-# detect trained objects in birds-eye view
-def detect_objects(input_bev_maps, model, configs):
-
-    # deactivate autograd engine during test to reduce memory usage and speed up computations
-    with torch.no_grad():  
-
-        # perform inference
-        outputs = model(input_bev_maps)
-
-        # decode model output into target object format
-        if 'darknet' in configs.arch:
-
-            # perform post-processing
-            output_post = post_processing_v2(outputs, conf_thresh=configs.conf_thresh, nms_thresh=configs.nms_thresh) 
-            detections = []
-            for sample_i in range(len(output_post)):
-                if output_post[sample_i] is None:
-                    continue
-                detection = output_post[sample_i]
-                for obj in detection:
-                    x, y, w, l, im, re, _, _, _ = obj
-                    yaw = np.arctan2(im, re)
-                    detections.append([1, x, y, 0.0, 1.50, w, l, yaw])    
-
-        elif 'fpn_resnet' in configs.arch:
-            # decode output and perform post-processing
-            
-            ####### ID_S3_EX1-5 START #######     
-            #######
-            print("student task ID_S3_EX1-5")
-
-            #######
-            ####### ID_S3_EX1-5 END #######     
-
-            
-
-    ####### ID_S3_EX2 START #######     
+    ####### ID_S1_EX2 START #######     
     #######
-    # Extract 3d bounding boxes from model response
-    print("student task ID_S3_EX2")
-    objects = [] 
+    print("student task ID_S1_EX2")
 
-    ## step 1 : check whether there are any detections
+    # step 1 : initialize open3d with key callback and create window
+    window = open3d.visualization.VisualizerWithKeyCallback()
+    window.create_window(window_name='Lidar point-cloud visualization')
 
-        ## step 2 : loop over all detections
-        
-            ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
-        
-            ## step 4 : append the current object to the 'objects' array
-        
-    #######
-    ####### ID_S3_EX2 START #######   
+    # step 2 : create instance of open3d point-cloud class
+    pcd = open3d.geometry.PointCloud()
+
+    # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
+    pcd.points = open3d.utility.Vector3dVector(pcl[:, :3])
+
+    # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
+    window.add_geometry(pcd)
+
+    # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
+    def key_callback(window):
+        window.close()
+        return False
     
-    return objects    
+    window.register_key_callback(262, key_callback)
+    window.run()
+    #######
+    ####### ID_S1_EX2 END #######     
+       
 
+# visualize range image
+def show_range_image(frame, lidar_name):
+
+    ####### ID_S1_EX1 START #######     
+    #######
+    print("student task ID_S1_EX1")
+
+    # step 1 : extract lidar data and range image for the roof-mounted lidar
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0]
+    ri = []
+    if len(lidar.ri_return1.range_image_compressed) > 0:
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
+
+    # step 2 : extract the range and the intensity channel from the range image
+    ri_range = ri[:,:,0]
+    ri_intensity = ri[:,:,1]
+
+    # step 3 : set values <0 to zero
+    ri_range[ri_range < 0] = 0
+    ri_intensity[ri_intensity < 0] = 0
+    
+    # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
+    ri_range = ri_range * 255 / (np.amax(ri_range) - np.amin(ri_range))
+    # ri_range = ri_range.astype(np.uint8)
+    
+    # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
+    percentile1 = np.percentile(ri_intensity, 1)
+    percentile99 = np.percentile(ri_intensity, 99)
+    ri_intensity = np.clip(ri_intensity, percentile1, percentile99) - percentile1
+    ri_intensity = ri_intensity * 255 / (percentile99 - percentile1)
+
+    # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
+    img_range_intensity = np.vstack((ri_range, ri_intensity)).astype(np.uint8)
+    deg90 = int(ri_range.shape[1] / 4)
+    ri_center = int(ri_range.shape[1] / 2)
+    img_range_intensity = img_range_intensity[:, ri_center - deg90:ri_center + deg90]
+    
+    #######
+    ####### ID_S1_EX1 END #######     
+    
+    return img_range_intensity
+
+
+# create birds-eye view of lidar data
+def bev_from_pcl(lidar_pcl, configs):
+
+    # remove lidar points outside detection area and with too low reflectivity
+    mask = np.where((lidar_pcl[:, 0] >= configs.lim_x[0]) & (lidar_pcl[:, 0] <= configs.lim_x[1]) &
+                    (lidar_pcl[:, 1] >= configs.lim_y[0]) & (lidar_pcl[:, 1] <= configs.lim_y[1]) &
+                    (lidar_pcl[:, 2] >= configs.lim_z[0]) & (lidar_pcl[:, 2] <= configs.lim_z[1]))
+    lidar_pcl = lidar_pcl[mask]
+    
+    # shift level of ground plane to avoid flipping from 0 to 255 for neighboring pixels
+    lidar_pcl[:, 2] = lidar_pcl[:, 2] - configs.lim_z[0]  
+
+    # convert sensor coordinates to bev-map coordinates (center is bottom-middle)
+    ####### ID_S2_EX1 START #######     
+    #######
+    print("student task ID_S2_EX1")
+
+    ## step 1 :  compute bev-map discretization by dividing x-range by the bev-image height (see configs)
+    discretization_step = (configs.lim_x[1] - configs.lim_x[0]) / configs.bev_height
+
+    ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates
+    lidar_pcl_cpy = np.copy(lidar_pcl)
+    lidar_pcl_cpy[:, 0] = np.int_(np.floor(lidar_pcl_cpy[:, 0] / discretization_step))
+
+    # step 3 : perform the same operation as in step 2 for the y-coordinates but make sure that no negative bev-coordinates occur
+    lidar_pcl_cpy[:, 1] = np.int_(np.floor(lidar_pcl_cpy[:, 1] / discretization_step) + (configs.bev_width + 1) / 2)
+
+    # step 4 : visualize point-cloud using the function show_pcl from a previous task
+    if visualize:
+        show_pcl(lidar_pcl_cpy)
+
+    #######
+    ####### ID_S2_EX1 END #######     
+    
+    
+    # Compute intensity layer of the BEV map
+    ####### ID_S2_EX2 START #######     
+    #######
+    print("student task ID_S2_EX2")
+
+    ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    intensity_map = np.zeros((configs.bev_height, configs.bev_width))
+
+    # step 2 : re-arrange elements in lidar_pcl_cpy by sorting first by x, then y, then -z (use numpy.lexsort)
+    lidar_pcl_cpy[lidar_pcl_cpy[:,3]>1.0, 3] = 1.0
+    idx_intensity = np.lexsort((-lidar_pcl_cpy[:, 2], lidar_pcl_cpy[:, 1], lidar_pcl_cpy[:, 0]))
+    lidar_pcl_cpy = lidar_pcl_cpy[idx_intensity]
+
+    ## step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
+    ##          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
+    _, idx_height_unique, counts = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True, return_counts=True)
+    lidar_pcl_top = lidar_pcl_cpy[idx_height_unique]
+
+    ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
+    ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
+    ##          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
+    percentile1 = np.percentile(lidar_pcl_top[:, 3], 1)
+    percentile99 = np.percentile(lidar_pcl_top[:, 3], 99)
+    intensity_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = (np.clip(lidar_pcl_top[:, 3], percentile1, percentile99) - percentile1) / (percentile99 - percentile1)
+
+    ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
+    if visualize:
+        intensity_image = intensity_map * 255
+        intensity_image = intensity_image.astype(np.uint8)
+        cv2.imshow('Intensity image', intensity_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    #######
+    ####### ID_S2_EX2 END ####### 
+
+
+    # Compute height layer of the BEV map
+    ####### ID_S2_EX3 START #######     
+    #######
+    print("student task ID_S2_EX3")
+
+    ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    height_map = np.zeros((configs.bev_height, configs.bev_width))
+
+    ## step 2 : assign the height value of each unique entry in lidar_top_pcl to the height map 
+    ##          make sure that each entry is normalized on the difference between the upper and lower height defined in the config file
+    ##          use the lidar_pcl_top data structure from the previous task to access the pixels of the height_map
+    height_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = lidar_pcl_top[:, 2] / float(configs.lim_z[1] - configs.lim_z[0])
+
+    ## step 3 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
+    if visualize:
+        height_image = height_map * 255
+        height_image = height_image.astype(np.uint8)
+        cv2.imshow('Height image', height_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    #######
+    ####### ID_S2_EX3 END #######       
+
+    # TODO remove after implementing all of the above steps
+    # lidar_pcl_cpy = []
+    # lidar_pcl_top = []
+    # height_map = []
+    # intensity_map = []
+
+    # Compute density layer of the BEV map
+    density_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
+    _, _, counts = np.unique(lidar_pcl_cpy[:, 0:2], axis=0, return_index=True, return_counts=True)
+    normalizedCounts = np.minimum(1.0, np.log(counts + 1) / np.log(64)) 
+    density_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = normalizedCounts
+        
+    # assemble 3-channel bev-map from individual maps
+    bev_map = np.zeros((3, configs.bev_height, configs.bev_width))
+    bev_map[2, :, :] = density_map[:configs.bev_height, :configs.bev_width]  # r_map
+    bev_map[1, :, :] = height_map[:configs.bev_height, :configs.bev_width]  # g_map
+    bev_map[0, :, :] = intensity_map[:configs.bev_height, :configs.bev_width]  # b_map
+
+    # expand dimension of bev_map before converting into a tensor
+    s1, s2, s3 = bev_map.shape
+    bev_maps = np.zeros((1, s1, s2, s3))
+    bev_maps[0] = bev_map
+
+    bev_maps = torch.from_numpy(bev_maps)  # create tensor from birds-eye view
+    input_bev_maps = bev_maps.to(configs.device, non_blocking=True).float()
+    return input_bev_maps
