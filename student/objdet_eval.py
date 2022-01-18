4b0237dea5d8eba800efbbb1b7b28cdd11e98b47
qiaunna <shcla13@morgan.edu>
2022-01-18 00:08:33 -0500

    Update objdet_eval.py

--- student/objdet_eval.py
+++ student/objdet_eval.py
@@ -68,8 +68,8 @@ def measure_detection_performance(detections, labels, labels_valid, min_iou=0.5)
                 ## step 4 : computer the center distance between label and detection bounding-box in x, y, and z
                 # remove item() when using groud_truth_label = True
                 # item() is used to cast tensor (x, y) as integers
-                dist_x = label.box.center_x-x.item()
-                dist_y = label.box.center_y-y.item()
+                dist_x = label.box.center_x-x
+                dist_y = label.box.center_y-y
                 dist_z = label.box.center_z-z
                 
                 ## step 5 : compute the intersection over union (IOU) between label and detection bounding-box
